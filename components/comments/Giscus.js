f9649d65b7c52e10b7c1995c228d1e821ea1ff14
Mason <65845572+bohemianmoon5@users.noreply.github.com>
2023-08-19 00:40:16 +0900

    Update Giscus.js

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -63,11 +63,8 @@ const Giscus = () => {
 
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
-      {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>}
-      <div className="giscus-container" id={COMMENTS_ID}>
-        <div className="giscus" style={{ margin: '0 auto' }} />
-      </div>  
-    /*<div className="giscus" id={COMMENTS_ID} />*/
+      {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>} 
+      <div className="giscus" id={COMMENTS_ID} />
     </div>
   )
 }
