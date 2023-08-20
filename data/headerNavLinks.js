d1aae89cb05011781b4b8fde2aed12640c51da8f
Mason <65845572+bohemianmoon5@users.noreply.github.com>
2023-08-20 23:09:25 +0900

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/guestbook', title: 'GuestBook' },
   { href: '/about', title: 'About' },
 ]
 
