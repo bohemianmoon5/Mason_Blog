f60d4676022dfe25d8ce258fdb8bc0b783235bd6
Mason <65845572+bohemianmoon5@users.noreply.github.com>
2023-08-22 00:03:47 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -22,9 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://bohemianmoon5.github.io/about">
-            About Mason Na
-          </Link>
+          <Link href="https://bohemianmoon5.github.io/about">About Mason</Link>
         </div>
       </div>
     </footer>
