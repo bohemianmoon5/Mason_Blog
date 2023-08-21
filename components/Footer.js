c1a58868bb27730f20cea91bf8b8147236930d61
Mason <65845572+bohemianmoon5@users.noreply.github.com>
2023-08-21 23:33:30 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -23,7 +23,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            About Mason
+            About Mason Na
           </Link>
         </div>
       </div>
