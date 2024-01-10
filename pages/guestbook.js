e3c860c6c607167b09ae312335b7f443d79abc84
Mason <bohemianmoon5@gmail.com>
2024-01-10 15:59:28 +0900

    change button color

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -55,9 +55,9 @@ const Guestbook = () => {
         <button
           onClick={uploadData}
           className="rounded-r-md bg-blue-500 px-4 py-2 text-lg text-white transition-all hover:bg-blue-600"
-          style={{ backgroundColor: '#7CD2B3' }}
+          style={{ backgroundColor: 'transparent' }}
         >
-          ⬇️
+          🚀
         </button>
       </div>
       <p className={`text-md text-red-500 ${emptyAlert ? 'block' : 'hidden'}`}>Message is empty</p>
