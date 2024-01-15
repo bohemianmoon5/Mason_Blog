ac0220290877ca7585dd105ca7de0580ebb58562
Mason Na <bohemianmoon5@gmail.com>
2024-01-15 23:52:46 +0900

    update

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,12 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
-import { getStaticProps } from '@/utils/api'
-
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
-const CodeBlock = ({ language, value }) => {
-  return (
-    <SyntaxHighlighter language={language} style={vscDarkPlus}>
-      {value}
-    </SyntaxHighlighter>
-  )
+export async function getStaticProps() {
+  const authorDetails = await getFileBySlug('authors', ['default'])
+  return { props: { authorDetails } }
 }
-
-export { getStaticProps }
 
 export default function About({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
@@ -25,7 +16,6 @@ export default function About({ authorDetails }) {
       layout={frontMatter.layout || DEFAULT_LAYOUT}
       mdxSource={mdxSource}
       frontMatter={frontMatter}
-      components={{ code: CodeBlock }}
     />
   )
 }
