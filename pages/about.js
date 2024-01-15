fa9f75940727040b06e4b685d064e008cad3d43a
Mason <bohemianmoon5@gmail.com>
2024-01-15 17:31:16 +0900

    update about

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,36 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
-import Intro from '../data/about/intro.mdx'
+import fs from 'fs/promises'
+import path from 'path'
 
-const DEFAULT_LAYOUT = 'AuthorLayout'
+const DEFAULT_AUTHOR_LAYOUT = 'AuthorLayout'
+const DEFAULT_ABOUT_LAYOUT = 'DefaultAboutLayout'
 
 export async function getStaticProps() {
   const authorDetails = await getFileBySlug('authors', ['default'])
-  return { props: { authorDetails } }
+
+  const introFilePath = path.join(process.cwd(), 'data', 'about', 'intro.mdx')
+
+  try {
+    const introContent = await fs.readFile(introFilePath, 'utf8')
+
+    const mergedMdxSource = `${authorDetails.mdxSource}\n\n${introContent}`
+
+    return { props: { authorDetails: { ...authorDetails, mdxSource: mergedMdxSource } } }
+  } catch (error) {
+    console.error(`Error reading intro.mdx: ${error}`)
+    return { props: { authorDetails } }
+  }
 }
 
 export default function About({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
 
   return (
-    <>
-      <Intro />
-
-      <MDXLayoutRenderer
-        layout={frontMatter.layout || DEFAULT_LAYOUT}
-        mdxSource={mdxSource}
-        frontMatter={frontMatter}
-      />
-    </>
+    <MDXLayoutRenderer
+      layout={frontMatter.layout || DEFAULT_ABOUT_LAYOUT}
+      mdxSource={mdxSource}
+      frontMatter={frontMatter}
+    />
   )
 }
