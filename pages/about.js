a03c842aadf5dfada66e92b154392482b73a2812
Mason <bohemianmoon5@gmail.com>
2024-01-15 17:46:04 +0900

    update

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,32 @@
+import { Metadata } from 'next'
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
-import fs from 'fs/promises'
-import path from 'path'
+import Intro from '../data/about/intro.mdx'
 
-const DEFAULT_AUTHOR_LAYOUT = 'AuthorLayout'
-const DEFAULT_ABOUT_LAYOUT = 'DefaultAboutLayout'
+const DEFAULT_LAYOUT = 'AuthorLayout'
 
 export async function getStaticProps() {
   const authorDetails = await getFileBySlug('authors', ['default'])
-
-  const introFilePath = path.join(process.cwd(), 'data', 'about', 'intro.mdx')
-
-  try {
-    const introContent = await fs.readFile(introFilePath, 'utf8')
-    console.log('introContent:', introContent)
-
-    const mergedMdxSource = `${authorDetails.mdxSource}\n\n${introContent}`
-    console.log('mergedMdxSource:', mergedMdxSource)
-
-    return { props: { authorDetails: { ...authorDetails, mdxSource: mergedMdxSource } } }
-  } catch (error) {
-    console.error(`Error reading intro.mdx: ${error}`)
-    return { props: { authorDetails } }
-  }
+  return { props: { authorDetails } }
 }
 
 export default function About({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
 
+  // Metadata constant
+  const metadata: Metadata = {
+    title: frontMatter.title || 'About Me',
+    description: frontMatter.description || 'Mason의 경력기술서',
+  }
+
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_ABOUT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <>
+      <MDXLayoutRenderer
+        layout={frontMatter.layout || DEFAULT_LAYOUT}
+        mdxSource={mdxSource}
+        frontMatter={frontMatter}
+      />
+      <Intro />
+    </>
   )
 }
