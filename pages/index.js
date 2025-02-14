0463600a460509c02417745dc635b7b384668455
Mason Na <bohemianmoon5@gmail.com>
2025-02-15 02:48:03 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import Typed from 'typed.js'
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
@@ -5,7 +7,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 3
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
@@ -14,18 +16,53 @@ export async function getStaticProps() {
 }
 
 export default function Home({ posts }) {
+  const el = React.useRef(null)
+  const typed = React.useRef(null)
+
+  React.useEffect(() => {
+    const options = {
+      strings: ['I love running', 'I love movies', 'I love music', 'I love myself'],
+      typespeed: 10,
+      backspeed: 10,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+    }
+
+    typed.current = new Typed(el.current, options)
+
+    return () => {
+      if(typed.current === 'function') {
+        typed.current.destory()
+      }
+    }
+  }, [])
+
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className="space-y-2 pt-6 pb-8 text-center md:space-y-5">
+          <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-6xl md:leading-14">
+            <span ref={el}> </span>
+          </h1>
+          <h1 className="mt-6 text-left text-3xl font-extrabold text-gray-700 dark:text-gray-300">
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
         </div>
+
+        {/* <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div> */}
+
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
