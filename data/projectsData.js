d85ac015472f76246bd2dd6c19c86206aa3ee653
bohemianmoon5 <bohemianmoon5@gmail.com>
2025-07-31 15:51:50 +0900

    update blog

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,10 +1,34 @@
 const projectsData = [
   {
-    title: 'Local Community Marketplace',
-    description: `This is my first team project. We are making Local Community Marketplace System on console. 
-    Main functions are direct dealing and safe transaction.`,
-    imgSrc: '/static/images/project1.png',
-    href: 'https://bohemianmoon5.github.io/project/my-first-project',
+    title: (
+      <>
+        현대홈쇼핑 SNOWFLAKE <br />
+        차세대 프로젝트<br />
+        (Hyundai Homeshopping)
+      </>
+    ),
+    description: `분석환경 구축을 위해 기존 On-Premise 환경을 Cloud 환경으로 전환`,
+    imgSrc: '/static/images/project5.jpg',
+    href: 'https://github.com/bohemianmoon5/Hyundai_Homeshopping_Snowflake_Project',
+  },
+  {
+    title: (
+      <>
+        LG U+ SNOWFLAKE <br />
+        금융마이데이터 프로젝트<br />
+        (LG U+)
+      </>
+    ),
+    description: `금융마이데이터 & 고객행동데이터 결합과 분석환경(Snowflake) 구축을 통한 대고객 마케팅 고도화`,
+    imgSrc: '/static/images/project4.jpg',
+    href: 'https://github.com/bohemianmoon5/LG_Uplus_Snowflake_Project',
+  },
+  {
+    title: 'Personal Blog',
+    description: `This is my first personal project. I am making this blog by Spring Framework using Java.
+    Main functions are user, board, reply, calendar.`,
+    imgSrc: '/static/images/project3.png',
+    href: 'https://github.com/bohemianmoon5/myblog',
   },
   {
     title: 'Kiosk System of Study Cafe',
@@ -14,11 +38,11 @@ const projectsData = [
     href: 'https://github.com/bohemianmoon5/CODERIUM',
   },
   {
-    title: 'Personal Blog',
-    description: `This is my first personal project. I am making this blog by Spring Framework using Java.
-    Main functions are user, board, reply, calendar.`,
-    imgSrc: '/static/images/project3.png',
-    href: 'https://github.com/bohemianmoon5/myblog',
+    title: 'Local Community Marketplace',
+    description: `This is my first team project. We are making Local Community Marketplace System on console. 
+    Main functions are direct dealing and safe transaction.`,
+    imgSrc: '/static/images/project1.png',
+    href: 'https://bohemianmoon5.github.io/project/my-first-project',
   },
 ]
 
